8ca76d1c684a2894cab130a211772659d168a9b9
Gregory <85207059+gpytak@users.noreply.github.com>
2023-05-02 18:53:39 -0700

    Update connect_four.py

--- connect_four.py
+++ connect_four.py
@@ -3,10 +3,75 @@
 
 from games import *
 
-class ConnectFour(TicTacToe):
-    def __init__(self, h=7, v=6, k=4):
-        TicTacToe.__init__(self, h, v, k)
+class ConnectFour(Game):
+    def __init__(self, h=6, v=7, k=4): #(h=6, v=7, k=4)
+        self.h = h
+        self.v = v
+        self.k = k
+        moves = [(x, y) for x in range(1, h + 1)
+                 for y in range(1, v + 1)]
+        self.initial = GameState(to_move='X', utility=0, board={}, moves=moves)
 
     def actions(self, state):
         return [(x, y) for (x, y) in state.moves
                 if x == self.h or (x + 1 , y ) in state.board]
+    
+    def result(self, state, move):
+        if move not in state.moves:
+            return state  # Illegal move has no effect
+        board = state.board.copy()
+        board[move] = state.to_move
+        moves = list(state.moves)
+        moves.remove(move)
+        return GameState(to_move=('O' if state.to_move == 'X' else 'X'),
+                         utility=self.compute_utility(board, move, state.to_move),
+                         board=board, moves=moves)
+
+    def utility(self, state, player):
+        """Return the value to player; 1 for win, -1 for loss, 0 otherwise."""
+        return state.utility if player == 'X' else -state.utility
+
+    def terminal_test(self, state):
+        """A state is terminal if it is won or there are no empty squares."""
+        return state.utility != 0 or len(state.moves) == 0
+
+    def display(self, board):
+        for x in range(1, self.h + 1):
+            for y in range(1, self.v + 1):
+                print(board.get((x, y), '.'), end=' ')
+            print()
+
+    def compute_utility(self, board, move, player):
+        """If 'X' wins with this move, return 1; if 'O' wins return -1; else return 0."""
+        if (self.k_in_row(board, move, player, (0, 1)) or
+                self.k_in_row(board, move, player, (1, 0)) or
+                self.k_in_row(board, move, player, (1, -1)) or
+                self.k_in_row(board, move, player, (1, 1))):
+            return +1 if player == 'X' else -1
+        else:
+            return 0
+
+    def k_in_row(self, board, move, player, delta_x_y):
+        """Return true if there is a line through move on board for player."""
+        (delta_x, delta_y) = delta_x_y
+        x, y = move
+        n = 0  # n is number of moves in row
+        while board.get((x, y)) == player:
+            n += 1
+            x, y = x + delta_x, y + delta_y
+        x, y = move
+        while board.get((x, y)) == player:
+            n += 1
+            x, y = x - delta_x, y - delta_y
+        n -= 1  # Because we counted move itself twice
+        return n >= self.k
+    
+if __name__ == "__main__":
+    connectfour = ConnectFour() # Creating the game instance
+    utility = connectfour.play_game(alpha_beta_player, query_player) # computer moves first
+    # utility = connectfour.play_game(query_player, alpha_beta_player) # player moves first
+    # utility = connectfour.play_game(query_player, query_player) # player vs player
+    if (utility < 0):
+        print("AI won the game")
+    else:
+        print("Player won the game")
