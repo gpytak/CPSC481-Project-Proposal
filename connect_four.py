9e205084d43d6e459283e29a7ac59f0b8f6e1ed7
Gregory <85207059+gpytak@users.noreply.github.com>
2023-05-08 13:30:57 -0700

    Update connect_four.py

--- connect_four.py
+++ connect_four.py
@@ -118,52 +118,6 @@ def score_position(board, piece):
 def is_terminal_node(board):
 	return winning_move(board, PLAYER_PIECE) or winning_move(board, AI_PIECE) or len(get_valid_locations(board)) == 0
 
-def minimax(board, depth, alpha, beta, maximizingPlayer):
-    valid_locations = get_valid_locations(board)
-    is_terminal = is_terminal_node(board)
-    
-    if depth == 0 or is_terminal:
-        if is_terminal:
-            if winning_move(board, AI_PIECE):
-                return (None, 100000000000000)
-            elif winning_move(board, PLAYER_PIECE):
-                return (None, -10000000000000)
-            else: # Game is over, no more valid moves
-                return (None, 0)
-        else: # Depth is zero
-            return (None, score_position(board, AI_PIECE))
-    if maximizingPlayer:
-        value = -math.inf
-        column = random.choice(valid_locations)
-        for col in valid_locations:
-            row = get_next_open_row(board, col)
-            b_copy = board.copy()
-            drop_piece(b_copy, row, col, AI_PIECE)
-            new_score = minimax(b_copy, depth-1, alpha, beta, False)[1]
-            if new_score > value:
-                value = new_score
-                column = col
-            alpha = max(alpha, value)
-            if alpha >= beta:
-                break
-        return column, value
-
-    else: # Minimizing player
-        value = math.inf
-        column = random.choice(valid_locations)
-        for col in valid_locations:
-            row = get_next_open_row(board, col)
-            b_copy = board.copy()
-            drop_piece(b_copy, row, col, PLAYER_PIECE)
-            new_score = minimax(b_copy, depth-1, alpha, beta, True)[1]
-            if new_score < value:
-                value = new_score
-                column = col
-            beta = min(beta, value)
-            if alpha >= beta:
-                break
-        return column, value
-
 def get_valid_locations(board):
 	valid_locations = []
 	for col in range(COLUMN_COUNT):
@@ -212,8 +166,6 @@ draw_board(board)
 pygame.display.update()
 myfont = pygame.font.SysFont("monospace", 75)
 
-
-
 def minmax_decision(board, depth, alpha, beta, maximizingPlayer):
     """Given a state in a game, calculate the best move by searching
     forward all the way to the terminal states. [Figure 5.3]"""
@@ -238,7 +190,7 @@ def minmax_decision(board, depth, alpha, beta, maximizingPlayer):
             row = get_next_open_row(board, col)
             b_copy = board.copy()
             drop_piece(b_copy, row, col, AI_PIECE)
-            new_score = minimax(b_copy, depth-1, alpha, beta, False)[1]
+            new_score = minmax_decision(b_copy, depth-1, alpha, beta, False)[1]
             if new_score > value:
                 value = new_score
                 column = col
@@ -253,7 +205,7 @@ def minmax_decision(board, depth, alpha, beta, maximizingPlayer):
             row = get_next_open_row(board, col)
             b_copy = board.copy()
             drop_piece(b_copy, row, col, PLAYER_PIECE)
-            new_score = minimax(b_copy, depth-1, alpha, beta, True)[1]
+            new_score = minmax_decision(b_copy, depth-1, alpha, beta, True)[1]
             if new_score < value:
                 value = new_score
                 column = col
@@ -435,4 +387,3 @@ if __name__ == "__main__":
     connectfour = ConnectFour()
     # utility = connectfour.play_game(minmax_player, query_player) # computer moves first
     utility = connectfour.play_game(query_player, minmax_player) # player moves first
-    # utility = connectfour.play_game(query_player, query_player) # player vs player
